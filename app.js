cb4c964f788c516bfbb198affead3cd71dd25bd2
AlexFerencz <ferenczalex99@gmail.com>
2024-04-11 19:06:22 +0100

    CSS done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = 3773;
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Database setup
 const db = new sqlite3.Database('./database/database.sqlite');
